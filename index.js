92e804a14ea82318025e8ee8fa6e2be8523ab212
Charissa Ramirez <chawissa@gmail.com>
2021-03-29 19:54:46 -0400

    One round

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+// Computer Choice
 function computerPlay() {
-  let compPick = ["rock", "paper", "scissors"];
-  let compGuess = compPick[Math.floor(Math.random() * compPick.length)];
-  return compGuess;
+  let compOptions = ["rock", "paper", "scissors"];
+  let compChoice = compOptions[Math.floor(Math.random() * compOptions.length)];
+  return compChoice;
 }
-console.log(computerPlay());
+
+// Single round
+function playRound(playerSelection, computerSelection) {
+  let result;
+  if (playerSelection === computerSelection) {
+    result = "It's a draw!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    result = "You won!";
+  } else {
+    result = "You lost";
+  }
+  console.log(result);
+}
+
+let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+let computerSelection = computerPlay();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
